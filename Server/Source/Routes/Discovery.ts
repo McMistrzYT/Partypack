5f178edfa6931e8773570112e3d8b4bd1b5039f0
AveryMadness <averyfd@shaw.ca>
2024-02-09 05:43:50 +0000

    add "all songs" category to tracks page

--- Server/Source/Routes/Discovery.ts
+++ Server/Source/Routes/Discovery.ts
@@ -14,9 +14,18 @@ App.get("/", async (req, res) => {
         Custom: false
     }
 
+    const All = {
+        ID: "all",
+        Header: "All Songs",
+        Songs: (await Song.find({where: {IsDraft: false}, order: {CreationDate: "DESC"}})).map(x => x.Package()),
+        Priority: 1000, //force to bottom, can be adjusted or fully fixed
+        Custom: false
+    }
+
     res.json([
         ...ForcedCategories.map(x => { return { ...x, Custom: true, Songs: x.Songs.map(y => y.Package()) }; }),
-        New
+        New,
+        All
     ].sort((a, b) => a.Priority - b.Priority))
 });
 
