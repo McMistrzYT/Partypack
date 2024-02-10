5085992f3e71954679a5e8660ddde6ad70c16138
absoluteSpacehead <absoluteSpacehead@gmail.com>
2024-02-10 01:53:31 +0000

    Fix new drafts with several published tracks

--- Server/Source/Routes/Drafting.ts
+++ Server/Source/Routes/Drafting.ts
@@ -48,7 +48,8 @@ App.post("/create",
         VocalsDifficulty: j.number().required().min(0).max(6)
     })),
     async (req, res) => {
-        if (req.user!.CreatedTracks.length >= Number(MAX_AMOUNT_OF_DRAFTS_AT_ONCE))
+        // what the actual fuck is wrong with mc
+        if (req.user!.CreatedTracks.filter(item => item.IsDraft == true).length >= Number(MAX_AMOUNT_OF_DRAFTS_AT_ONCE))
             return res.status(400).send("You ran out of free draft spots. Please delete some first.");
 
         const SongData = await Song.create({
